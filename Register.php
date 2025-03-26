fa8ab312b90973f7b7410379c3d69bc39913290e
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2025-03-26 18:19:40 +0700

    Update Register.php

--- Register.php
+++ Register.php
@@ -30,7 +30,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Form</title>
+    <title>Form Student</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
